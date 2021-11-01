b9b96218cc3f5b4c258dcae6d1601ffa8ddf91a8
kianghong1998 <kianghong998@gmail.com>
2021-11-01 08:19:59 -0700

    changed a little bit

--- Codings/RainWater.py
+++ Codings/RainWater.py
@@ -4,5 +4,3 @@ inches_int = int(inches_str)
 volume = (inches_int/12)*43560
 gallons = volume*7.48051945
 print (inches_int, "in. rain on 1 arce is ", gallons, "gallons")
-inches_float = float(inches_str)
-print (inches_float, "in. rain on 1 arce is ", gallons, "gallons")
