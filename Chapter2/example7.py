9795fc4a1d54eddf42a8261b49def8e3e2ad974b
kianghong1998 <kianghong998@gmail.com>
2021-11-17 00:29:58 -0800

    uploaded 17Nov

---
+++ Chapter2/example7.py
@@ -0,0 +1,21 @@
+number = 50
+# get an initial guess
+guess_str = input ("Guess a number: ")
+guess = int(guess_str)
+
+# while guess is range, keep asking
+while 0 <= guess <= 100:
+    if guess > number:
+        print ("Guessed Too High.")
+    elif guess < number:
+        print ("Guessed Too Low.")
+    else:
+        print ("You guessed it. The number was: ", number)
+        break
+
+    #keep going, get the next guess
+    guess_str = input("Guess a number: ")
+    guess = int(guess_str)
+
+else:
+    print ("You quit early, the number was:", number)
